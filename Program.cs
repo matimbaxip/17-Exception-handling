120654e45b8c3cd878db453d2d3b192a188cd8c9
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-09 22:04:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_Exception_handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Code that may throw an exception
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                Console.ReadLine();
+            }
+
+        }
+    }   
+}
